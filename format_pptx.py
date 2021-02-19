1488588805da3d8b95f52ada84af67dfb5c5bdab
Billy Buehl <wbuehl@ndc.nasa.gov>
2021-02-19 11:10:29 -0500

    moved filters to separate functions

--- format_pptx.py
+++ format_pptx.py
@@ -57,6 +57,7 @@ class Course:
         self._pptx_file = f
 
     def get_pptx_dict(self):
+        # return dictionary of pptx
         pptx = []
         for n, _ in enumerate(self.pres):
             pptx.append({
@@ -66,27 +67,21 @@ class Course:
             })
         return pptx
 
-    def get_notes(self):
+    def get_notes(self, *kwargs):
         # return list of tuples: [(page_num, notes), (...)]
         narration = []
         
+        # iterate through pptx slides
         for n, _ in enumerate(self.pres.slides):
             slide_text = self.get_slide_text(n+1)
             slide_notes = self.get_slide_notes(n+1)
+
+            # apply filters
+            for k in kwargs:
+                slide_notes = k(slide_notes, slide_text, n+1)
+
+            # add slide notes to narration list
             if slide_notes:
-                # omit knowlege checks
-                if slide_text.lower().replace(' ', '').startswith('knowledgecheck'):
-                    print(f'Slide {str(n+1)} | Knowledge Check skipped: {slide_notes}')
-                    continue
-                    
-                # omit slide notes after "Aditional Information"
-                if 'Additional Information' in slide_notes:
-                    split_notes = slide_notes.split('Additional Information')
-                    if split_notes[0].replace('\n', ''):
-                        narration.append((n+1, split_notes[0].replace('\n', '')))
-                    print(f'slide {str(n+1)} | Additional Information skipped: ' + split_notes[1].replace('\n', ''))
-                    continue
-                    
                 narration.append((n+1, slide_notes.replace('\n', '')))
 
         return narration
@@ -119,6 +114,23 @@ class Course:
 
         return None
 
+
+def filter_kc(slide_notes, slide_text, slide_num):
+    # remove knowlege checks
+    if slide_text.lower().replace(' ', '').startswith('knowledgecheck'):
+        print(f'Slide {str(slide_num)} | Knowledge Check skipped: {slide_notes}')
+        return ''
+    return slide_notes
+
+def filter_ai(slide_notes, _, slide_num):
+    # omit slide notes after "Aditional Information"
+    if 'Additional Information' in slide_notes:
+        split_notes = slide_notes.split('Additional Information')
+        if split_notes[0].replace('\n', ''):
+            print(f'slide {slide_num} | Additional Information skipped: ' + split_notes[1].replace('\n', ''))
+            return split_notes[0].replace('\n', '')
+        return ''
+    return slide_notes
 
 def prevent_doc_breakup(document):
   tags = document.element.xpath('//w:tr')
@@ -178,5 +190,5 @@ if __name__ == '__main__':
     # pres_file = r'C:\Users\wbuehl\Documents\python_stuff\powerpoint_automation\SMA-SS-WBT-0013_RIDM.pptx'
 
     course = Course(pres_file, 'HQ108')
-    course_notes = course.get_notes()
+    course_notes = course.get_notes(filter_kc, filter_ai)
     mk_narration_docx(course.course_id, course.file_id, course.course_title, course_notes)
